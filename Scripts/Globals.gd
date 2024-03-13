8d7192097171aff3ca9371476d86fb72c4c9f88a
mdue1337 <martin@familiendue.dk>
2024-03-13 12:54:39 +0100

    database

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -1,3 +1,10 @@
 extends Node
 
-var fullName = "error"
+var fullName : String = "error"
+var password : String = "test";
+var email : String  = "test@gmail.com";
+var userId : int = 1;
+var gameId : int;
+var money : int;
+var days : int;
+var upgrades : int;
--- database.gd
+++ database.gd
@@ -14,52 +14,24 @@ func _ready():
 	randomize()
 	add_child(http_request)
 	http_request.connect("request_completed",self,"_http_request_completed")
-	get_leaderboard()
-
+	get_user()
 
 func _process(delta):
 	if is_requesting:
 		return
-		
+	
 	if request_queue.empty():
 		return
-		
+	
 	is_requesting = true
+	_send_request(request_queue.pop_front())
 	
-	if nonce == null:
-		request_nonce()
-	else:
-		_send_request(request_queue.pop_front())
-	
-	
-func request_nonce():
-	var client = HTTPClient.new()
-	var data = client.query_string_from_dict({"data" : JSON.print({})})
-	var body = "command=get_nonce&" + data
-	
-	var err = http_request.request(SERVER_URL, SERVER_HEADERS, true, HTTPClient.METHOD_POST, body)
-	
-	if err != OK:
-		printerr("HTTPRequest error: " + String(err))
-		return
-		
-	print("Requeste nonce")
-	
-		
 func _send_request(request: Dictionary):
 	var client = HTTPClient.new()
 	var data = client.query_string_from_dict({"data" : JSON.print(request['data'])})
 	var body = "command=" + request['command'] + "&" + data
 	
-	var cnonce = String(Crypto.new().generate_random_bytes(32)).sha256_text()
-	
-	var client_hash = (nonce + cnonce + body + String(SECRET_KEY)).sha256_text()
-	print(client_hash)
-	nonce = null
-	
 	var headers = SERVER_HEADERS.duplicate()
-	headers.push_back("cnonce: " + cnonce)
-	headers.push_back("hash: " + client_hash)
 	
 	var err = http_request.request(SERVER_URL, headers, true, HTTPClient.METHOD_POST, body)
 		
@@ -78,21 +50,20 @@ func _http_request_completed(_result, _response_code, _headers, _body):
 		return
 	
 	var response_body = _body.get_string_from_utf8()
-	
-	#$TextEdit.set_text(response_body)
 	var response = parse_json(response_body)
+	print(response)
+	## handle the response.
 
 func create_user():
 	var command = "add_user_login";
-	var password = String("password".to_utf8()).sha256_text()
-	var data = {"email": 'test@gmail.com', "test": 'hans', "secret": password}
+	var password = String(Gs.password.to_utf8()).sha256_text()
+	var data = {"email": String(Gs.email), "name": String(Gs.fullName), "secret": password}
 	request_queue.push_back({"command" : command, "data" : data})
 	
 func get_user():
 	var command = "get_user_login";
-	var password = String("password".to_utf8()).sha256_text()
-	print(password)
-	var data = {"email": 'test@gmail.com', "secret": password}
+	var password = String(Gs.password.to_utf8()).sha256_text()
+	var data = {"email": String(Gs.email), "secret": password}
 	request_queue.push_back({"command" : command, "data" : data})
 
 func get_leaderboard():
@@ -102,19 +73,20 @@ func get_leaderboard():
 	
 func add_user_to_leaderboard():
 	var command = "add_user_to_leaderboard"
-	var userId = 1 # change
-	var gameId = 1 # change
-	var data = {"userId": userId, "gameId": gameId}
+	var data = {"userId": int(Gs.userId), "gameId": int(Gs.gameId)}
 	request_queue.push_back({"command" : command, "data" : data})
 
 func create_game():
 	var command = "create_game"
-	var userId = int(1);
-	var data = {"userId": userId};
+	var data = {"userId": int(Gs.userId)};
 	request_queue.push_back({"command" : command, "data" : data})
 
 func load_game():
-	request_queue.push_back({})
+	var command = "load_game"
+	var data = {"userId": int(Gs.userId)}
+	request_queue.push_back({"command" : command, "data" : data})
 
 func update_game():
-	request_queue.push_back({})
+	var command = "update_game"
+	var data = {"userId": int(Gs.userId), "money": int(Gs.money), "days": int(Gs.days), "upgrades": int(Gs.upgrades)}
+	request_queue.push_back({"command" : command, "data" : data})
