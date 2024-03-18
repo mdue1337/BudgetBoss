8aecf450d0e6eb8544a4048425c61d6f0e3dde65
mdue1337 <martin@familiendue.dk>
2024-03-18 14:55:12 +0100

    Hej

--- Scripts/BillsButton.gd
+++ Scripts/BillsButton.gd
@@ -2,4 +2,4 @@ extends TextureButton
 
 func _on_Bills_pressed():
 	get_owner().get_node("Pay Bills").visible = true
-	$Label4.text = "You have: " + str(Gs.bills) + "$ in bills"
+	get_owner().get_node("Pay Bills").get_node("PayAll/Label2").text = "You have: " + str(Gs.bills) + "$ in bills"
--- Scripts/Pay_Bills.gd
+++ Scripts/Pay_Bills.gd
@@ -1,12 +1,9 @@
 extends TextureButton
 
-func _ready():
-	$Label2.text = "You have: " + str(Gs.bills) + "$ in bills"
-
 func _on_PayAll_pressed():
 	if(Gs.moneyBank > Gs.bills):
 		Gs.moneyBank -= Gs.bills
 		Gs.bills = 0
-		$Label2.text = "You paid your bills. You currently have: " + str(Gs.bills) + "$ in bills"
+		$Label2.text = "You paid your bills. \nYou currently have: " + str(Gs.bills) + "$ in bills"
 	else:
-		$Label2.text = "You don't have sufficient funds. However, you still have " + str(Gs.bills) + "$ in bills"
+		$Label2.text = "You don't have sufficient funds. \nHowever, you still have " + str(Gs.bills) + "$ in bills"
