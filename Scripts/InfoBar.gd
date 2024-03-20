b06e455bf7d483ce0b05df6f4dde13dd9cfebadd
benj7617 <116954841+benj7617@users.noreply.github.com>
2024-03-20 13:02:29 +0100

    importeret en masse fonts og lavet globalbar

--- Scripts/InfoBar.gd
+++ Scripts/InfoBar.gd
@@ -8,20 +8,23 @@ func _ready():
 	var Cash = get_node("TextureRect/HBoxContainer/Cash");
 	var Bank = get_node("TextureRect/HBoxContainer/Bank");
 	var Day = get_node("TextureRect/HBoxContainer/Day");
+	var Hunger = get_node("TextureRect/HBoxContainer/Hunger");
 	
 	Cash.text = "Cash: " + str(Gs.moneyPerson)
 	Bank.text = "Bank: " + str(Gs.moneyBank)
 	Day.text = "Day: " + str(Gs.days)
+	Hunger.text = "Hunger: " + str(Gs.hunger)
 	
 	print(Cash.text)
 	print(Bank.text)
 	print(Day.text)
+	print(Hunger.text)
 
 	# Resize the fields to fit the text
-	resize_fields([Cash, Bank, Day])
+	resize_fields([Cash, Bank, Day, Hunger])
 	
 	# Move the labels within the HBoxContainer
-	move_labels([Cash, Bank, Day])
+	move_labels([Cash, Bank, Day, Hunger])
 
 func resize_fields(fields):
 	for field in fields:
@@ -32,5 +35,6 @@ func move_labels(labels):
 	for label in labels:
 		print(label.rect_position.x)
 		label.rect_position.x = position_x
-		position_x += label.rect_min_size.x + 100  # Add some padding between labels
+		position_x += label.rect_min_size.x + 150;
 		print(label.rect_position.x)
+		
