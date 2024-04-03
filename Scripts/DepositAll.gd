85ad3045bda8065226751c415f48422a6b2d43ab
mdue1337 <martin@familiendue.dk>
2024-04-03 13:48:42 +0200

    #12 #11 should be done

--- Scripts/DepositAll.gd
+++ Scripts/DepositAll.gd
@@ -1,12 +1,16 @@
 extends TextureButton
 
 func _on_DepositAll_pressed():
-	print("moneyBank:" + str(Gs.moneyBank))
-	print("moneyPerson:" + str(Gs.moneyPerson))
+	if Gs.moneyPerson == 0:
+		var popup = get_parent().get_parent().get_node("Win")
+		popup.dialog_text = "You need to have more than 0 dollars on you to deposit \n"
+		popup.visible = true
+	else:
+		print("moneyBank:" + str(Gs.moneyBank))
+		print("moneyPerson:" + str(Gs.moneyPerson))
+		Gs.moneyBank += Gs.moneyBank
+		Gs.moneyPerson = 0
+		print("transfered")
+		print("moneyBank:" + str(Gs.moneyBank))
+		print("moneyPerson:" + str(Gs.moneyPerson))
 	
-	Gs.moneyBank = Gs.moneyBank + Gs.moneyPerson
-	Gs.moneyPerson = 0
-	
-	print("transfered")
-	print("moneyBank:" + str(Gs.moneyBank))
-	print("moneyPerson:" + str(Gs.moneyPerson))
