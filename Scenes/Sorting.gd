da374bc41d6fae5805ae8d1c05a91602117970f0
Aayan0909 <Aayanminhas0909@gmail.com>
2024-04-05 13:19:54 +0200

    Sorting game done

--- Scenes/Sorting.gd
+++ Scenes/Sorting.gd
@@ -1,100 +1,58 @@
 extends Node2D
 
+var nuts = 5
+var bolts = 5
+
 var selected = false
-
-
-func _ready():
-	pass # Replace with function body.
 
 func _process(delta):
 	if selected:
 		followMouse()
 
-func followMouse():
-	position = get_global_mouse_position()
-
-func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		if event.pressed:
-			$Nut.selected = true
-			$Bolt.selected = true
-		else:
-			$Nut.selected = false
-			$Bolt.selected = false
-
-
-func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		if event.pressed:
-			selected = true
-			print("pressed")
+func _input(event):
+	if Input.is_action_just_pressed("mouse_left"):
+		var l = global_position - get_global_mouse_position()
+		if l.length() < 35:
+			selected = !selected
+			print("selected")
 		else:
 			selected = false
-			print("released")
+
+func followMouse():
+	global_position = get_global_mouse_position()
+
+func _on_NutBox_area_entered(area):
+	if str(area).begins_with("Nut"):
+		nuts -= 1
+		if nuts < 0:
+			nuts = 0
+		print("Nuts left: " + str(nuts))
+		is_game_won()
+
+func _on_BoltBox_area_entered(area):
+	if str(area).begins_with("Bolt"):
+		bolts -= 1
+		if bolts < 0:
+			bolts = 0
+		print("Bolts left: "+str(bolts))
+		is_game_won()
 
 
-
-""""
-extends Node2D
-
-
-const NUM_NUTS = 10
-const NUM_BOLTS = 10
-
-
-onready var NutBox = $NutBox
-onready var BoltBox = $BoltBox
-
-
-func _on_NutBox_body_entered(body):
-
-	if body.name.contains("Nut"):
-		body.sort_into_box(NutBox)
-
-func _on_BoltBox_body_entered(body):
-
-	if body.name.contains("Bolt"):
-		body.sort_into_box(BoltBox)
-
-
-func sort_into_box(box):
-	
-	get_parent().remove_child(self)
-	queue_free()
-
-	
-	box.add_child(self)
-
-
-func get_random_spawn_position():
-
-	return Vector2(rand_range(0, 100), rand_range(0, 100))
-
-func spawn_nuts_and_bolts():
-
-	for i in range(NUM_NUTS):
-		spawn_random("nut")
-
-	
-	for i in range(NUM_BOLTS):
-		spawn_random("bolt")
-
-func spawn_random(type):
-	
-	var position = get_random_spawn_position()
-
-	# Duplicate the nut or bolt node
-	var node
-	if type == "nut":
-		node = $Nut.duplicate()
+func is_game_won():
+	if nuts == 0 and bolts == 0:
+		get_node("GameWin").visible = true
+		get_tree().paused = true
 	else:
-		node = $Bolt.duplicate()
+		pass
 
-	# Set the node position and add it to the scene
-	node.position = position
-	add_child(node)
 
-func _ready():
-	spawn_nuts_and_bolts()
-"""
+func _on_GameWin_confirmed():
+	Gs.emit_signal("workpay",1)
+	get_tree().change_scene("res://Scenes/Factory.tscn")
+	
+
+
+
+
+
 
