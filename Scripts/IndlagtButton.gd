b7f2d221f29497e305f3b1b8b765ef399805e398
benj7617 <116954841+benj7617@users.noreply.github.com>
2024-04-10 15:10:21 +0200

    det virker

--- Scripts/IndlagtButton.gd
+++ Scripts/IndlagtButton.gd
@@ -6,21 +6,23 @@ func _ready():
 		get_parent().get_node("Hospital").visible = true
 		get_parent().get_node("Node2D2").visible = false
 		get_parent().get_node("Accident").visible = true
+		get_parent().get_node("Accidentstuff").visible = true
 
-func _on_Accident_confirmed():
+func _on_Medicin_pressed():
 	if (Gs.medicineCount > 0):
 		Gs.accident = false;
 		Gs.medicineCount -= 1;
 		get_tree().change_scene("res://Scenes/Map.tscn");
 
-func _on_Accident_visibility_changed():
-		Gs.emit_signal("dayCounter");
-		Gs.emit_signal("dayCounter");
-		Gs.emit_signal("dayCounter");
-		Gs.accident = false;
-		Gs.bills += 500;
-		get_tree().change_scene("res://Scenes/Hospital.tscn");
+func _on_Dage_pressed():
+	Gs.emit_signal("dayCounter");
+	Gs.emit_signal("dayCounter");
+	Gs.emit_signal("dayCounter");
+	Gs.accident = false;
+	Gs.bills += 500;
+	get_tree().change_scene("res://Scenes/Hospital.tscn");
 
+#legacy kode udkommenteret
 func _on_Hospital_pressed():
 	pass
 	#if (Gs.illnessLevel > 0):
