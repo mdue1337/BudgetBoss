2564955ae26b300b1bac9f852b586182ec591fb5
mdue1337 <martin@familiendue.dk>
2024-04-10 15:04:08 +0200

    push

--- Scripts/Pay_Bills.gd
+++ Scripts/Pay_Bills.gd
@@ -2,7 +2,7 @@ extends TextureButton
 
 func _on_PayAll_pressed():
 	if(Gs.moneyBank > Gs.bills):
-		Gs.moneyBank -= Gs.bills
+		Gs.emit_signal("bankCounter", -Gs.bills)
 		Gs.bills = 0
 		$Label2.text = "You paid your bills. \nYou currently have: " + str(Gs.bills) + "$ in bills"
 	else:
