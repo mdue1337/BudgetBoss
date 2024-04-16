f27d9643bd5affcd83e5257a12ff1c6f50d37c80
benj7617 <116954841+benj7617@users.noreply.github.com>
2024-04-16 14:36:33 +0200

    Implementeret baggrundsmusik

--- Scripts/VideoPlayer.gd
+++ Scripts/VideoPlayer.gd
@@ -3,6 +3,7 @@ extends VideoPlayer
 func _ready():
 	if Gs.newPLayer == false:
 		hide()
+	BackgroundMusic.get_node("AudioStreamPlayer").playing = false
 
 func _on_VideoPlayer_finished():
 	hide()
@@ -14,3 +15,4 @@ func hide():
 	Gs.newPLayer = false
 	get_parent().get_node("VideoPlayer").visible = false
 	get_parent().get_node("VideoPlayer").paused = true
+	BackgroundMusic.get_node("AudioStreamPlayer").playing = true
